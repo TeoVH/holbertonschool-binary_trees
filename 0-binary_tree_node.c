5442dfa6b0a061c75833fb0abe94899c40aa228d
TeoVH <villadamateo64@gmail.com>
2022-07-26 13:58:14 -0500

    Files for the 0 point

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,6 +1,15 @@
-#include "binary_tress.h"
+#include "binary_trees.h"
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    
+    binary_tree_t *create_node = malloc(sizeof(binary_tree_t));
+    if (create_node == NULL)
+    {
+        return(NULL);
+    }
+    create_node->parent = parent;
+    create_node->n = value;
+    create_node->left = NULL;
+    create_node->right = NULL;
+    return (create_node);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -1,9 +1,9 @@
-#include <stdlib.h>
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
 #include <stdio.h>
-
-/* PROTOTYPES */
-
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -13,12 +13,16 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
+/* Prototypes */
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif /* _BINARY_TREES_H_ */

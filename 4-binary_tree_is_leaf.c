365a8bbb8833b2f052561c35638aae9142f34186
TeoVH <villadamateo64@gmail.com>
2022-07-26 22:42:56 -0500

    Point 4 working, betty necesary

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (!node)
+    {
+        return(0);
+    }
+    
+    if (node != NULL && !node->left && !node->right)
+    {
+        return(1);
+    }
+
+    return(0);
+}

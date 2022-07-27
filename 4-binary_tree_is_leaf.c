74f0b892ccabf4e3553cf45e1cc035309e5a1c4e
felipeholbi <4758@holbertonschool.com>
2022-07-26 22:49:18 -0500

    4_betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,16 +1,23 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_leaf - check the leaf
+ *
+ * @node: passed parent
+ * Return: 1 if is leafe 0 otherwise
+ */
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (!node)
-    {
-        return(0);
-    }
-    
-    if (node != NULL && !node->left && !node->right)
-    {
-        return(1);
-    }
+	if (!node)
+	{
+		return (0);
+	}
 
-    return(0);
+	if (node != NULL && !node->left && !node->right)
+	{
+		return (1);
+	}
+
+	return (0);
 }

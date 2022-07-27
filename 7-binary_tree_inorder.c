31bf0d93e95bf0ffcad158361cbd6c494251d49e
felipeholbi <4758@holbertonschool.com>
2022-07-27 10:18:31 -0500

    7-inorder-traversal

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - In-order traversal
+ *
+ * @tree: the root
+ * @func: the ponter to function pass n
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
+

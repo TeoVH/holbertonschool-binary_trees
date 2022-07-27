f1e5e67d2beccaf75591a4e2601e3236baa11fd9
felipeholbi <4758@holbertonschool.com>
2022-07-26 23:00:56 -0500

    5_betty_check

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,11 +1,23 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_root - check the root
+ *
+ * @node: the root
+ * Return: 1 if is root or 0 otherwise
+ */
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node != NULL && node->left && node->right)
-    {
-        return(1);
-    }
+	if (!node)
+	{
+		return (0);
+	}
 
-    return(0);
+	if (node != NULL && node->left && node->right)
+	{
+		return (1);
+	}
+
+	return (0);
 }

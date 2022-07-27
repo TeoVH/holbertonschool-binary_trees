fb07edd55666f56326519e73c5db4465ab2cfb8f
felipeholbi <4758@holbertonschool.com>
2022-07-27 10:29:09 -0500

    fix_1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,25 +1,34 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_left -  Insert left
+ *
+ * @parent: pointer to node
+ * @value: value of node
+ * Return: new node
+ */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *insert_left = NULL;
+	binary_tree_t *insert_left = NULL;
 
-    if (!parent)
-    {
-        return(NULL);
-    }
-    insert_left = malloc(sizeof(binary_tree_t));
-    if (!insert_left)
-    {
-        return (NULL);
-    }
-    insert_left->n = value;
-    insert_left->parent = parent;
-    if (parent->left)
-    {
-        insert_left->left = parent->left;
-        insert_left->left->parent = insert_left;
-    }
-    parent->left = insert_left;
-    return(insert_left);
+	if (!parent)
+		return (NULL);
+
+	insert_left = malloc(sizeof(binary_tree_t));
+
+	if (!insert_left)
+		return (NULL);
+
+	insert_left->n = value;
+	insert_left->parent = parent;
+
+	if (parent->left)
+	{
+		insert_left->left = parent->left;
+		insert_left->left->parent = insert_left;
+	}
+
+	parent->left = insert_left;
+	return (insert_left);
 }

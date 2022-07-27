60c112a9e741dec5493e41b8f205b06876e4dd8c
felipeholbi <4758@holbertonschool.com>
2022-07-26 22:29:25 -0500

    3_betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,14 +1,20 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - delete nodes
+ *
+ * @tree: root
+ */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (!tree)
-    {
-        return;
-    }
+	if (!tree)
+	{
+		return;
+	}
 
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 
-    free(tree);
+	free(tree);
 }

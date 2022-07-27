138368e4c80e4b939da766d891bd57399a31ab0d
felipeholbi <4758@holbertonschool.com>
2022-07-27 11:15:51 -0500

    1_fixed_variables

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,23 +10,27 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *insert_left = malloc(sizeof(binary_tree_t));
+	binary_tree_t *insert_left = NULL;
 
 	if (parent == NULL || value == '\0')
 		return (NULL);
+
+	insert_left = malloc(sizeof(binary_tree_t));
 
 	if (insert_left == NULL)
 		return (NULL);
 
 	insert_left->n = value;
 	insert_left->parent = parent;
+	insert_left->right = NULL;
+	insert_left->left = NULL;
 
 	if (parent->left)
 	{
 		insert_left->left = parent->left;
 		insert_left->left->parent = insert_left;
 	}
-
 	parent->left = insert_left;
+
 	return (insert_left);
 }

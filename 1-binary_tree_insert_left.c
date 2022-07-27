ab310939efaa6af7e0d3a73da70218e8ba134357
felipeholbi <4758@holbertonschool.com>
2022-07-27 10:46:02 -0500

    fixes_conditions

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *insert_left = malloc(sizeof(binary_tree_t));
 
 	if (!insert_left || !parent)
+	{
 		return (NULL);
+	}
 
 	insert_left->n = value;
 	insert_left->parent = parent;

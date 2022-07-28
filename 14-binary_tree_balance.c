d6ca17d10b986caa76cf71be97563d372b3129f7
felipeholbi <4758@holbertonschool.com>
2022-07-28 18:21:12 -0500

    14_measure the factor balancer

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance -  the balance factor
+ *
+ * @tree: pointer to nod
+ * Return: the measure balancer factor of df
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int df = 0, count_left = 0, count_right = 0;
+
+	if (tree->left)
+	{
+		count_left += 1 + binary_tree_balance(tree->left);
+	}
+
+	if (tree->right)
+	{
+		count_right += 1 + binary_tree_balance(tree->right);
+	}
+
+	df = count_left - count_right;
+	return (df);
+}
